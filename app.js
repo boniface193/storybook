87d7e86c5ce0daeb0b2340144b483af81d358654
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-30 14:01:19 +0100

    updated changes complated

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
